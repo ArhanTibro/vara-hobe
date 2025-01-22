90ebc21deead5db3e408e7f5882f890c4fdd1e59
Arhan Tibro <arhantibro2004@gmail.com>
2025-01-22 11:48:34 +0600

    test6

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,7 +1,7 @@
 function App() {
   return (
     <>
-      <h1 className="text-6xl text-orange-500 text-center">Hello Project !</h1>
+      <h1 className="text-6xl text-orange-500 text-center">Hello Project!</h1>
     </>
   );
 }
--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 Vara-Hobe
+Primary-color = #3F4651
+secondary-color = #C0BCB5
+Tertiary-color = #EBECED
